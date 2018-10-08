9d77f576a808a7664d9b2facfd29bf3417dcc3ab
terahara ayumu <terahara.ayumu@gmail.com>
2018-10-08 18:36:51 +0900

    [tweek] 不要な宣言

--- src/pages/account_list.js
+++ src/pages/account_list.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
 import Card from "@material-ui/core/Card";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
