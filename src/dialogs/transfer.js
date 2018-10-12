82acd5f35002ed1f0ca55af079e387a45135230e
terahara ayumu <terahara.ayumu@gmail.com>
2018-10-12 22:00:30 +0900

    [add] 振込

--- src/dialogs/transfer.js
+++ src/dialogs/transfer.js
@@ -8,13 +8,47 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { withStyles } from "@material-ui/core/styles";
 
+import config from "../config";
+
 const styles = theme => {};
 class TransferDialog extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      from_account_number: "",
+      to_bank_code: "",
+      to_branch_code: "",
+      to_account_number: "",
+      amount: "",
+      memo: "",
+      isConfirm: false,
+      transfer_id: ""
+    };
   }
-
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+  handleTransfer = () => {
+    const headers = {
+      Authorization: "Bearer test",
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    };
+    const body = JSON.stringify({ ...this.state });
+    fetch(`${config.backend.baseUri}/transfer`, {
+      method: "POST",
+      body,
+      headers
+    })
+      .then(res => (res.ok ? res.json() : new Error(res.json())))
+      .then(json => {
+        this.setState({ transfer_id: json.transfer_id });
+        this.setState({ isConfirm: true });
+      })
+      .catch(e => console.log(e));
+  };
   render() {
     return (
       <Dialog
@@ -31,6 +65,8 @@ class TransferDialog extends React.Component {
             id="from_account_number"
             label="振込元口座番号"
             type="string"
+            value={this.state.from_account_number}
+            onChange={this.handleChange("from_account_number")}
             fullWidth
           />
           <TextField
@@ -39,6 +75,8 @@ class TransferDialog extends React.Component {
             id="to_bank_code"
             label="振込先金融機関コード"
             type="string"
+            value={this.state.to_bank_code}
+            onChange={this.handleChange("to_bank_code")}
             fullWidth
           />
           <TextField
@@ -47,6 +85,8 @@ class TransferDialog extends React.Component {
             id="to_branch_code"
             label="振込先支店コード"
             type="string"
+            value={this.state.to_branch_code}
+            onChange={this.handleChange("to_branch_code")}
             fullWidth
           />
           <TextField
@@ -54,6 +94,8 @@ class TransferDialog extends React.Component {
             margin="dense"
             id="to_account_number"
             label="振込先口座番号"
+            value={this.state.to_account_number}
+            onChange={this.handleChange("to_account_number")}
             type="string"
             fullWidth
           />
@@ -63,6 +105,8 @@ class TransferDialog extends React.Component {
             id="amount"
             label="振込金額"
             type="string"
+            value={this.state.amount}
+            onChange={this.handleChange("amount")}
             fullWidth
           />
           <TextField
@@ -70,6 +114,8 @@ class TransferDialog extends React.Component {
             id="memo"
             label="備考"
             type="string"
+            value={this.state.memo}
+            onChange={this.handleChange("memo")}
             fullWidth
           />
         </DialogContent>
@@ -77,8 +123,8 @@ class TransferDialog extends React.Component {
           <Button onClick={this.props.handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={this.props.handleClose} color="primary">
-            振込
+          <Button onClick={this.handleTransfer} color="primary">
+            確認
           </Button>
         </DialogActions>
       </Dialog>
